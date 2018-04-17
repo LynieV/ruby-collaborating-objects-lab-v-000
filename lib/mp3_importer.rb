4474330ca9aac07adae063eeab9f5bf8c82afc6d
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-17 19:24:35 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,6 +14,7 @@ class MP3Importer
     self.files.each do |file|
       song = Song.new_by_filename(file)
       Artist.all << song.artist unless Artist.all.include?(song.artist)
+    end
   end
   
 end
